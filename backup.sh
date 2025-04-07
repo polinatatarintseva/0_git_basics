1f7b35f063ca256cb39fd0835a06e89d5e73cb15
polinatatarintseva <pollytat2005@gmail.com>
2025-04-07 21:05:26 +0300

    Added backup function to backup.sh

--- backup.sh
+++ backup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 echo "This is a backup script."
 # This is a comment
+
+backup_files() {
+    local source_dir=$1
+    local dest_dir=$2
+    cp -r "$source_dir"/* "$dest_dir"
+    echo "Backup of $source_dir completed."
+}
